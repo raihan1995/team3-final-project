f68fb711320d13c5ceea9e0bc473bc6cc637c181
brownlessdaniel1 <DBrownless@qa.com>
2021-05-03 07:40:43 +0000

    db: minor edits to frontend-test

--- frontend-test.sh
+++ frontend-test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sudo apt install npm
-npm uninstall -g angular-cli @angular/cli e clean
-sudo npm cache clean
-sudo npm install -g @angular/cli@latest
-npm install --save-dev @angular/cli@latest
-sudo apt install karma-tools
-npm install --save-dev @angular-devkit/build-angular
+sudo apt update -y
+sudo apt install npm -y
+sudo npm uninstall -g angular-cli @angular/cli e clean
+sudo npm cache clean --force
+echo n | sudo npm install -g @angular/cli@8.0.3
+echo n | sudo npm install --save-dev @angular/cli@8.0.3
+sudo npm install --save-dev @angular-devkit/build-angular@0.803.24
 cd spring-petclinic-angular-master
-npm install
-ng test
+echo n | npm install
+sudo apt install chromium-browser
+export CHROME_BIN='/usr/bin/chromium-browser'
+ng test --watch false --browsers ChromeHeadless
