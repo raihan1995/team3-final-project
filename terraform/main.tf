a01496d8fe5bd41d2e243ac907517de69b918a94
RobLewisQA <rlewis@academytrainee.com>
2021-05-04 15:02:16 +0000

    RL - added to terraform folder and content in files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,23 @@
 #provider
 
 module "ec2" {
-  source         = "./ec2"
-  net_id         = module.subnets.net_id
-  ami_id         = ##########################
-  instance_type  = "t2.micro"
-  av_zone        = "eu-west-2a"
-  key_name       = #########################
-
+  source             = "./ec2"
+  net_id             = module.subnets.net_id
+  ami_id             = ##########################
+  instance_type      = "t2.micro"
+  av_zone            = "eu-west-2a"
+  key_name           = #########################
 }
-module "subnets" {
 
+module "subnets" {
+  source             =
+  vpc_id             =
+  route_id           =
+  security_group_id  =
+  net_private_ips    =
+  internet_gateway   =
     
 }
 module "VPC" {
-
-    
+  source             = "./vpc"
 }
