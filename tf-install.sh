b68f0125016dd5f9b0fcf42222dec4f30191d8ce
brownlessdaniel1 <DBrownless@qa.com>
2021-05-04 13:48:33 +0000

    db: added more terraform stuff

--- tf-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y unzip wget
-wget https://releases.hashicorp.com/terraform/0.15.1/terraform_0.15.1_linux_amd64.zip
-unzip terraform_*_linux_*.zip
-sudo mv terraform /usr/local/bin
-rm terraform_*_linux_*.zip
-terraform --version
